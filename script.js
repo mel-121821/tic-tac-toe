4b5e9fb83686b20dbf34422533d2bedeb1cb74a8
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2024-10-24 07:12:36 -0400

    Add logic to clickHandlerBoard() to prevent click events outside of cells. Remove console.logs and unused code

--- script.js
+++ script.js
@@ -22,32 +22,22 @@ const gameBoard = (function() {
             board[i].push("");
         }
     }
-
-    // let board = [["X","O","X"],["O","O","X"],["O","X",""]];
     
-
     let moveValid = true;
-    // console.log(moveValid);
 
     const markBoard = (marker, arrayRow, arrayColumn) => {
-        if (board[arrayRow][arrayColumn] == "") {
+        if (board[arrayRow][arrayColumn] === "") {
         board[arrayRow][arrayColumn] = `${marker}`;
-            console.log(board);
             moveValid = true;
         } else {
             // do nothing
             moveValid = false;
-            console.log(board);
-            console.log("There is a marker here already")
         }
     };
 
     const checkValidMove = () => moveValid;
-    
 
     const arrayMatch = (array) => {
-        // console.log(array);
-        // console.log(array[0]);
         if (array.every(val => val === array[0]) && (array[0] !== "")) {
             threeWayMatch = true
         } else {
@@ -62,7 +52,7 @@ const gameBoard = (function() {
             allBoardCombos.push(row)
             }
         for (let c = 0; c < board[0].length; c++) {
-            let col = board.map(function(value,index) { return value[c];});
+            let col = board.map(function(value) { return value[c];});
             allBoardCombos.push(col);
         }
         allBoardCombos.push([board[0][0], board[1][1], board[2][2]]);
@@ -77,70 +67,46 @@ const gameBoard = (function() {
     const checkBoard = () => {
         checkAvailableSpaces();
         getAllBoardCombos();
-        // console.log(allBoardCombos);
         for (combo of allBoardCombos) {
             arrayMatch(combo);
             if (threeWayMatch === true) {
                 break
             }
-            // console.log(threeWayMatch)
         }
         resetAllBoardCombos();
     }
 
     const checkAvailableSpaces = () => {
-        // boardFull = false;
         function spaceCheck(board) {
         boardFull = board.every(row => row.every(cell => cell === "X" || cell === "O"));
         }
         spaceCheck(board);
-        // console.log(boardFull)
-        // console.log(board)
         return boardFull;
     }
 
     const clearBoard = () => {
         for (let i = 0; i < board.length; i++) {
-            // console.log(board.length)
-            // console.log(board[i])
             for (let j = 0; j < board[i].length; j++) {
-                // console.log(board[i].length)
-                // console.log(board[i][j]);
                 board[i][j] = "";
             }
         }
-        
-        console.log(board);
     }
 
     const getBoard = () => board;
 
-
-    // checkBoard();
-
     return {
-        // move checkValidMove, CheckBoard to gameController?
         markBoard, checkValidMove, checkBoard, clearBoard, getBoard
     }
 })();
-
-// console.log(gameBoard.markBoard("X", 0, 0));
-// console.log(gameBoard.markBoard("O", 0, 0));
-// console.log(gameBoard.markBoard("M", 2, 2))
-
-
 
 
 // ____________Players________________
 
 const Players = function() {
-   
-    // let playerOne = ""
-    // let playerTwo = ""
 
     const players = [
         {
-            name: "Player one",
+            name: "Player One",
             marker: "X"
         },
         {
@@ -151,10 +117,8 @@ const Players = function() {
 
     function addPlayerNames(input1, input2) {
         obj1 = players.findIndex(obj => obj.marker === "X")
-        console.log(players[obj1]);
         players[obj1].name = input1;
         obj2 = players.findIndex(obj => obj.marker === "O")
-        console.log(players[obj2]);
         players[obj2].name = input2;
     }
 
@@ -164,18 +128,13 @@ const Players = function() {
 }();
 
 
-
-
 // __________Game Flow______________
-
 
 function gameController(){ 
     let activePlayer = Players.getPlayers()[0];
-    // console.log(activePlayer);
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === Players.getPlayers()[0] ? Players.getPlayers()[1] : Players.getPlayers()[0];
-        // console.log(activePlayer);
     }
 
     const setToPlayerOne = () => {
@@ -189,46 +148,27 @@ function gameController(){
     }
 
     function playRound(inputRow, inputColumn) {
-        console.log(`${getActivePlayer().name} marks the board...`)
         placeMarker(inputRow, inputColumn);
-        // console.log(gameBoard.checkValidMove());
         if (gameBoard.checkValidMove() == true) {
             gameBoard.checkBoard();
-            console.log(`Is there a winner? ${threeWayMatch}`);
-            console.log(`Is the board full? ${boardFull}`)
             checkForWinner();
         } else {
-            // do nothing
-            console.log("Please choose a different square")
-            printNewRound();
+            // do nothing, player has not marked a valid square
         }
-    }
-
-    const printNewRound = () => {
-        console.log(`${getActivePlayer().name}'s turn`);
     }
 
     const checkForWinner = () => {
-        if (threeWayMatch === true) {
-            console.log(`${getActivePlayer().name} wins!`)
-            // gameBoard.clearBoard();
-        } else if (threeWayMatch === false && boardFull === true) {
-            console.log("Tie game!")
-            console.log(boardFull)
-            // gameBoard.clearBoard();
+        if (threeWayMatch === true || boardFull === true) {
+            // do nothing, the game is ended
         } else {
             switchPlayerTurn();
-            printNewRound();
         }
     }
-
-    // printNewRound();
 
     return {getActivePlayer, playRound, getBoard: gameBoard.getBoard, setToPlayerOne}
 
 };
 
-// const game = gameController();
 
 // __________Display Control______________
 
@@ -262,17 +202,9 @@ function screenController() {
         const board = game.getBoard();
         const activePlayer = game.getActivePlayer();
 
-        // console.log(board[0][0]);
-        // console.log(threeWayMatch);
-        // gameBoard.checkBoard();
-
-        // display player's turn
-        // playerDiv.textContent = `${activePlayer.name}'s turn...`;
-
         const playerDisplay = () => {
             gameBoard.checkBoard();
             if (threeWayMatch === true) {
-                console.log(threeWayMatch)
                 playerDiv.textContent = `${activePlayer.name} wins!`;
              } else if (threeWayMatch === false && boardFull === true) {
                  playerDiv.textContent = "Tie game!"
@@ -283,7 +215,6 @@ function screenController() {
         playerDisplay();
 
         board.forEach(row => {
-            // console.log(board.indexOf(row))
             row.forEach((cell, index) => {
                 //anything clickable should be a button
                 const cellButton = document.createElement("button");
@@ -292,30 +223,23 @@ function screenController() {
                 cellButton.dataset.row = board.indexOf(row);
                 cellButton.dataset.column = index;
                 cellButton.textContent = cell;
-                // console.log(cell[index])
-                // console.log(cell)
                 boardDiv.appendChild(cellButton);
             }) 
         })
     }
 
-    // function boardDisable() {
-    //     cellButton.disabled = true
-    // }
-
     function clickHandlerBoard(e) {
         const selectedCellRow = e.target.dataset.row;
         const selectedCellCol = e.target.dataset.column;
-        console.log(selectedCellRow, selectedCellCol)
         // make sure the columnn is clicked and not the gaps in between
-        // if (!selectedCell) return;
+        if (!selectedCellCol) return;
         game.playRound(selectedCellRow, selectedCellCol);
         updateScreen();
     }
 
     boardDiv.addEventListener("click", function(e) {
         if (threeWayMatch === true) {
-            // do nothing
+            // do nothing, game has ended
         } else {
             clickHandlerBoard(e)
         }
@@ -324,97 +248,12 @@ function screenController() {
     resetButton.addEventListener("click", () => {   
         gameBoard.clearBoard();
         game.setToPlayerOne()
-        // gameBoard.checkBoard();
-        // playerDisplay();
         updateScreen();
-        console.log("clicked");
         formPopup.showModal();
     });
 
     // initial render
     updateScreen();
-    
 }
 
 screenController();
-
-// __________Get Player Data______________
-
-
-
-
-
-
-
-//______________Unused code_____________
-
-// function createPlayer(name, marker) {
-    // const name = name;
-    // const marker = marker
-    // You don't need to define name and marker with const. You are defining them when you return the object with the name and marker
-
-    // const steve = createPlayer("Steve", "X");
-// console.log(steve.name);
-// console.log(steve.marker);
-// console.log(steve.placeMarker(0, 0))
-// console.log(steve.placeMarker(0, 1))
-
-// const dustin = createPlayer("Dustin", "O");
-// console.log(dustin.name);
-// console.log(dustin.marker);
-// console.log(dustin.placeMarker(1, 0))
-// console.log(dustin.placeMarker(1, 0))
-
-//________________CheckAvailableSpaces Code Brainstorming________________
-
-// const checkAvailableSpaces = () => {
-//     boardFull = false;
-//     function spaceCheck(board) {
-//     boardFull = board.every(row => row.every(cell => cell === "X" || cell === "O"));
-//     }
-//____________Working Function Above_________
-
-// ______________Unused Code Below__________
-//     spaceCheck(board);
-//     console.log(spaceCheck(board))
-//     // let spaceCheck = (row => row.every(cell => console.log(indexOf(cell))));
-//     // console.log(board.every(spaceCheck))
-//     // if (spaceCheck.board === true) {
-//     //     boardFull = true;
-//     // } else {
-//     //     boardFull = false
-//     // }
-//     console.log(boardFull)
-//     // const boardSpace = (space) => 
-//     //     space === "X" || space === "O";
-//     // // should return true
-//     //boardFull = board.some(x => x === "");
-//     // console.log(board.some(boardSpace))
-//     // console.log(boardSpace)
-//     // console.log(boardFull);
-//     console.log(board)
-//     // board.every(function (i) {
-//     //     if (i === "X" || i === "O") {
-//     //         boardFull = true
-//     //     } else {
-//     //         boardFull = false
-//     //     }
-
-//     // })
-//     // for (let i = 0; i < board.length; i++) {
-//     //     for (let j = 0; j < board[i].length; j++) {
-//     //         // console.log(board)
-//     //         if (board[i][j] === "") {
-//     //             boardFull = false;
-//     //             // break;
-//     //         } else {
-//     //             console.log(board[i][j])
-//     //             boardFull = true;
-//     //             console.log(boardFull);
-//     //         }
-//     //     }
-//     // }
-//     return boardFull;
-//     // console.log(board)
-//     // console.log(boardFull)
-// }
